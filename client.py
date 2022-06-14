5f302a3c3727b3bac6c061d3e206a3cf8687fd75
wawan-ikhwan <ikhwanperwira@gmail.com>
2022-06-14 23:44:47 +0700

    mantap 8-bit

--- client.py
+++ client.py
@@ -3,19 +3,51 @@ import numpy as np
 from time import time
 from mss import mss
 import pyautogui as pag
+import socket
 
-resolution = pag.size()
-monitor = {"top": 0, "left": 0, "width": resolution[0], "height": resolution[1]}
+# Client Config
+addrPortServer = ('127.0.0.1', 20001)
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocket.settimeout(1)
+
+oldBytesFrame = np.fromfile('./waiting-server.jpg', dtype=np.uint8)
+
+# screenSize = pag.size()
+screenSize = (640, 480)
+
 lastDelay = None
 updateDelayTime = time()
 with mss() as sct:
   while True:
     loopTime = time()
 
+    UDPClientSocket.sendto(b'?', addrPortServer)
+    bytesFrame = b''
+    lenBytesFromServer = 508
+    try:
+      while lenBytesFromServer == 508:
+        bytesFromServer = UDPClientSocket.recvfrom(65507)[0]
+        lenBytesFromServer = len(bytesFromServer)
+        bytesFrame += bytesFromServer
+    except:
+      bytesFrame = oldBytesFrame
+
+    # print(len(bytesFrame))
+
+    try:
+      frame = cv.imdecode(np.frombuffer(bytesFrame, dtype=np.uint8), 1)
+      frame = cv.resize(frame,screenSize)
+    except Exception as e:
+      print(e)
+      frame = cv.imdecode(np.frombuffer(oldBytesFrame, dtype=np.uint8), 1)
+      frame = cv.resize(frame, screenSize)
+
+
     # FPS Management
     currentDelay = (time() - loopTime)
     if time() - updateDelayTime > 1:
-      lastDelay = 1 / currentDelay
+      lastDelay = currentDelay
+      print(len(bytesFrame))
       updateDelayTime = time()
     frame = cv.putText(frame, 
       text = str(lastDelay)[:6], 
@@ -31,6 +63,7 @@ with mss() as sct:
     if cv.waitKey(1) == ord('q'):
       cv.destroyAllWindows()
       break
+      
+    oldBytesFrame = bytesFrame
 
 print('Done.')
-print(resolution)
--- server.py
+++ server.py
@@ -5,14 +5,31 @@ from mss import mss
 import pyautogui as pag
 import socket
 
+monitor = pag.size()
+
+# Optimization Config
+'''
+2160p = (3840, 2160)
+1440p = (2560, 1440)
+1080p = (1920, 1080)
+720p  = (1280, 720)
+480p  = (640, 480)
+360p  = (480, 360)
+240p  = (426, 240)
+144p  = (256, 144)
+'''
+# resolution = monitor # Uncomment for using server resolution
+resolution = (256, 144) # Uncomment for using common resolution
+compression = 5 # 100 means no compress, 0 is highest compression!
+
 # Server Config
 addrPortServer = ('127.0.0.1', 20001)
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind(addrPortServer)
 UDPServerSocket.settimeout(2)
+payloadSize = 508
 
-resolution = pag.size()
-monitor = {"top": 0, "left": 0, "width": resolution[0], "height": resolution[1]}
+monitor = {"top": 0, "left": 0, "width": monitor[0], "height": monitor[1]}
 
 print('UDP Server is running...')
 with mss() as sct:
@@ -27,12 +44,11 @@ with mss() as sct:
     if addrPortClient is not None:
       frame = np.asarray(sct.grab(monitor))
       frame = cv.cvtColor(frame, cv.COLOR_BGRA2BGR)
-      encodeParam = (int(cv.IMWRITE_JPEG_QUALITY), 25)
+      frame = cv.resize(frame, dsize=resolution)
+      encodeParam = (int(cv.IMWRITE_JPEG_QUALITY), compression)
       isSucceed, encImg = cv.imencode('.jpg', frame, encodeParam)
-      UDPServerSocket.sendto(encImg.tobytes(), addrPortClient)
-
-    if cv.waitKey(1) == ord('q'):
-      break
-
-print('Done.')
-print(resolution)
+      bytesToSend = encImg.tobytes()
+      # print(len(bytesToSend))
+      listBytesToSend = [bytesToSend[i:i+payloadSize] for i in range(0, len(bytesToSend), payloadSize)]
+      for b in listBytesToSend:
+        UDPServerSocket.sendto(b, addrPortClient)
